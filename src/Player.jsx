f2527351c59564fed088c39534ccf0c0a64614c4
Fabian <fabian@Big-Mac-de-Fabian.local>
2023-11-10 18:42:01 +0100

    camera and lights ok

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,7 +1,8 @@
 import { useRapier, RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import * as THREE from "three";
 
 
 
@@ -12,6 +13,9 @@ export default function Player()
     const [ subscribeKeys, getKeys ] = useKeyboardControls()
     const { rapier, world } = useRapier()
     const rapierWorld = world
+
+    const [ smoothedCameraPosition ] = useState(new THREE.Vector3(10, 10, 10))
+    const [ smoothedCameraTarget ] = useState(new THREE.Vector3())
 
     const jump = () =>
     {
@@ -78,6 +82,27 @@ export default function Player()
 
         body.current.applyImpulse(impulse)
         body.current.applyTorqueImpulse(torque)
+
+        /**
+         * Camera
+         */
+
+        const bodyPosition = body.current.translation()
+        const cameraPosition = new THREE.Vector3()
+        cameraPosition.copy(bodyPosition)
+        cameraPosition.y += 0.65
+        cameraPosition.z += 2.25
+
+        const cameraTarget = new THREE.Vector3()
+        cameraTarget.copy(bodyPosition)
+        cameraTarget.y += 0.25
+
+        smoothedCameraPosition.lerp(cameraPosition, 5 * delta)
+        smoothedCameraTarget.lerp(cameraTarget, 5 * delta)
+
+        state.camera.position.copy(smoothedCameraPosition)
+        state.camera.lookAt(smoothedCameraTarget)
+
 
     })
 
